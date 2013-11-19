67d042084e4e2ca531c34c7b8190994050e30f9b
Steven Tseng <booda406@gmail.com>
2013-11-19 14:43:24 +0800

    Initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+	def fancybox
+		
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fancybox::Application.config.secret_key_base = '8c28c0c6a61bc53718b51a78aa9c59a138d8d7c3c9f30596fd3fcec8ea4fcdf63bc6afbfc413a7ba07c4c44dc251f8a8b77a17ce03cf2887993791c53a58a10d'
